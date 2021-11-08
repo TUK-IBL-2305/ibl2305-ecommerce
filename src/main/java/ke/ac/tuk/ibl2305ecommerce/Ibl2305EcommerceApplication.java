d0d830465a369d900dda2f8ab5c72b0c50f94756
Richard Omoka <richard@thehillcroftgroup.com>
2021-11-08 08:26:03 +0300

    Initial commit New project

---
+++ src/main/java/ke/ac/tuk/ibl2305ecommerce/Ibl2305EcommerceApplication.java
@@ -0,0 +1,13 @@
+package ke.ac.tuk.ibl2305ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ibl2305EcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ibl2305EcommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ke/ac/tuk/ibl2305ecommerce/Ibl2305EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package ke.ac.tuk.ibl2305ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ibl2305EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
